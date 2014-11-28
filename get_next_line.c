4af15b1845351203c838037c644ba3d26e3de266
Mike Mercier <merciermk@MacBook-Pro-de-Mike-7.local>
2014-11-28 21:52:01 +0100

    hello

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mmercier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/24 13:15:57 by mmercier          #+#    #+#             */
-/*   Updated: 2014/11/27 19:51:35 by mmercier         ###   ########.fr       */
+/*   Updated: 2014/11/28 12:08:52 by mmercier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "./libft/libft.h"
 
 
-static void			ft_cut(char **line, char **save)
+static void			ft_cut(char **save, char **line)
 {
 	size_t			len;
 	char			*tmp;
@@ -33,31 +33,38 @@ static void			ft_cut(char **line, char **save)
 		ft_strdel(save);
 		return (0);
 	}
-
+/*
+static int			ft_error(char **save)
+{
+	if (*save)
+		ft_strdel(save);
+	return (-1);
+}
+*/
 
 int	get_next_line(int const fd, char **line)
 {
 	static char	*save;
 	char		*tmp;
-	int			r;
+	int		r;
 	char		buf[BUFF_SIZE + 1];
 
-	r = 1;
-
-	if (fd == -1 || BUFF_SIZE <= 0 || r == -1 || tmp == NULL)
+	r = 0;
+	save = NULL;
+	tmp = NULL;
+	
+	if (fd == -1 || BUFF_SIZE <= 0 || r == -1)
 		return (-1);
 	if (save == NULL)
 		save = ft_strnew(BUFF_SIZE + 1);
 	while ((ft_strchr(save, '\n') == NULL) && ((r = read(fd, buf, BUFF_SIZE)) > 0))	
 	{
 		buf[r] = '\0';
-		tmp = malloc(sizeof(char) * ft_strlen(save) + BUFF_SIZE);
 		tmp = save;
 		save = ft_strjoin(tmp, buf);
 		free(tmp);
 	}
-	if (fd == 0)
-		return(0);
+
 	if (r < BUFF_SIZE && ft_strchr(save, '\n') == NULL)
 		return (ft_eof(line, &save));
 	if (r == -1)
