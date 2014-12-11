c0907691b1593541e182f351af4d169d7c66b5af
Mercier Mike <merciermk@free.fr>
2014-12-11 10:24:45 +0100

    libft

---
+++ libft/ft_itoa.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmercier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/19 14:31:09 by mmercier          #+#    #+#             */
+/*   Updated: 2014/11/20 13:22:56 by mmercier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_itoa(int n)
+{
+	char		*ret;
+	int			temp_n;
+	size_t		size_ret;
+	char		sign;
+
+	sign = (n < 0) ? -1 : 1;
+	size_ret = 2 + (n < 0);
+	temp_n = n;
+	while ((n = n / 10))
+		size_ret++;
+	n = temp_n;
+	if ((ret = (char *)malloc(sizeof(char) * size_ret--)) == NULL)
+		return (NULL);
+	ret[size_ret--] = '\0';
+	ret[size_ret--] = sign * (n % 10) + '0';
+	while ((n = n / 10))
+		ret[size_ret--] = sign * (n % 10) + '0';
+	if (sign < 0)
+		ret[size_ret] = '-';
+	return (ret);
+}
